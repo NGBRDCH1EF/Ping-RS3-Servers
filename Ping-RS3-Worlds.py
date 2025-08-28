b680b1b7807043acfa488e5d5adb5df5ed9fa902
NGBRDCH1EF <oliverio.nicholas@gmail.com>
2025-08-28 16:11:02 -0700

    added feature

--- Ping-RS3-Worlds.py
+++ Ping-RS3-Worlds.py
@@ -2,7 +2,7 @@ import subprocess
 import platform
 import re
 from collections import deque
-print("this is feature branch")
+import csv
 
 valid_worlds = (1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,91,92,94,96,97,98,99,100,101,102,103,104,105,106,108,114,115,116,117,118,119,120,121,122,123,124,134,135,136,137,138,139,140,141,142,144,145,176,177,178,179,180,181,182,183,184,185,200,201,202,203,204,205,206,207,210,211,212,213,214,215,216,217,218,219,220,221,225,226,227,228,229,235,236,237,238,239,245,246,247,249,250,251,252,255,256,257,258,259)
 # valid_worlds = (1,2,3,4,5,6,7,8,9,10)
@@ -44,12 +44,6 @@ def ping_world(world,count):
 
     return None
 
-
-def pingall(count):
-    results = {}
-    for world in valid_worlds:
-        results[world] = ping_world(world,count)
-    return results
 
 def ping_all_par(count):
     processes = []
@@ -103,12 +97,10 @@ def menu():
 
     # Special cases
     if world_choice.lower() == "a":
-        ping_results = ping_all_par(count)              
+        ping_results = ping_all_par(count)
+        write_to_csv(ping_results)              
         print_results(ping_results)
         return True
-
-    
-    
 
     try:
         world = int(world_choice)
@@ -119,6 +111,7 @@ def menu():
 
         else:
             stats = ping_world(world,count)
+            write_to_csv({world: stats})
             print_results({world: stats})  
             return True
 
@@ -128,14 +121,24 @@ def menu():
 
 
 def print_results(results):
-    print("\nWorld | Min | Avg | Max")
-    print("-" * 25)
+    print("\nWorld | Min | Max | Avg     (ms)")
+    print("-" * 31)
     for world, stats in results.items():
         if stats:
-            print(f"{world:<5} | {stats['min']:<3} | {stats['avg']:<3} | {stats['max']:<3}")
+            print(f"{world:<5} | {stats['min']:<3} | {stats['max']:<3} | {stats['avg']:<3}")
         else:
             print(f"{world:<5} | unreachable")
 
+def write_to_csv(results):
+    with open("ping_data.csv", "w", newline="") as file:
+        writer = csv.writer(file)
+        writer.writerow(["World", "Min", "Avg", "Max"])
+        for world, stats in results.items():
+            if stats:
+                writer.writerow([world,stats["min"],stats["max"],stats["avg"]])
+            else:
+                writer.writerow([world, "unreachable"])
+        
 
 # How I found list of valid worlds:
 # def find_reachable(start=1, end=259):
@@ -146,12 +149,6 @@ def print_results(results):
 #             reachable.append(world)
 #     return reachable
 
-
-# def check_valid():
-#     valid_worlds = find_reachable(1, 259)
-#     print(valid_worlds)
-#     print(f"There are {len(valid_worlds)} valid worlds.")
-        
 
 while menu():
     pass
